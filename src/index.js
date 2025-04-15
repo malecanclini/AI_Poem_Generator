5d0f88f0dc1517e96e85e0216d94544c36a2c54b
malecanclini <166962846+malecanclini@users.noreply.github.com>
2025-04-15 18:37:47 -0300

    api integration

--- src/index.js
+++ src/index.js
@@ -11,9 +11,9 @@ function displayPoem(response) {
     event.preventDefault();
   
     let instructionsInput = document.querySelector("#user-instructions");
-    let apiKey = "2046c535afeb092fo82f1d306d8a2b2t";
+    let apiKey = "24c2d34edtb05a9b0ao32170dd17e08f4";
     let context =
-      "You are a romantic Poem expert and love to write short poems. You mission is to generate a 4 line poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning";
+      "You are a romantic french poem expert and love to write short poems. You mission is to generate a 4 line poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning";
     let prompt = `User instructions: Generate a French poem about ${instructionsInput.value}`;
     let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
   
